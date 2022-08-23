17ac687b15725a43f6965f0dde253151bb1b29a1
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2022-08-23 10:04:55 -0400

    init commit

---
+++ src/Components/Hello.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Hello() {
+  return (
+    <div>
+      {/* write anything you want, between here and the closing div tag.  Be fancy or not, I don't care. */}
+      <h2>Hi Michael!</h2>
+      <h1>Write me a message!</h1>
+      <p>
+        You can write these things in html tags like h1, h2, p,
+        whatever. (You might have noticed this is within a paragraph
+        tag)
+      </p>
+      Or just write stuff without any html tags or styling. I truly
+      don't care. (This isn't in any tag -- just text. Totally fine.)
+      <h3>Your turn! :-)</h3>
+      {/* Just don't go beyond this line.  For now.  Eventually you'll go far beyond these borders.  But not now. */}
+    </div>
+  );
+}
+
+export default Hello;
